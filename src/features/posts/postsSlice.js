34ae29a0a9c18254bb1ef65af7ddcaa5af7249fc
D <gdongcode@gmail.com>
2022-04-07 00:32:59 +0800

    fix: 增加点赞功能

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,34 +1,70 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' },
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello!',
+    user: '0',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      hooray: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0,
+    },
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    user: '2',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      hooray: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0,
+    },
+  },
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    // postAdded(state, action) {
-    //   state.push(action.payload)
-    // }, 这样写具体对状态的操作逻辑分散在各个组件中 需要内聚道状态管理
-
     postAdded: {
-        reducer(state, action) {
-            state.push(action.payload)
-        },
-        prepare (title,content,userId) {
-            // 利用 prepare 高内聚具体的数据加工步骤
-            return {
-                payload: {
-                    id: nanoid(),
-                    title,
-                    content,
-                    user: userId
-
-                }
-            }
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+            reactions: {
+              thumbsUp: 0,
+              hooray: 0,
+              heart: 0,
+              rocket: 0,
+              eyes: 0,
+            },
+          },
         }
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
@@ -41,5 +77,6 @@ const postsSlice = createSlice({
   },
 })
 
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+
 export default postsSlice.reducer
